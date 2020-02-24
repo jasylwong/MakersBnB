d23a96181fb544456c1a5e7687a072bb7b513235
Jason <jasylwong@gmail.com>
2020-02-24 15:27:51 +0000

    setup add space feature test

--- app.rb
+++ app.rb
@@ -8,13 +8,22 @@ set :database, "sqlite3:project-name.sqlite3"
 
 class MakersBnB < Sinatra::Base
   get '/' do
-    "MakersBnB"
+    File.read('./views/index.html')
   end
 
+  get '/spaces' do
+    "space_1 Space Mansion £50 Lovely space!"
+  end
 
-get '/spaces' do
-  "space_1"
-end
+  post '/spaces' do
+    redirect('/spaces')
+  end
+
+  get '/spaces/new' do
+    erb :spaces_new
+  end
+
+  run! if app_file == $0
 end 
 
 require './models'
--- spec/features/testing_signing_up_spec.rb
+++ spec/features/testing_signing_up_spec.rb
@@ -3,5 +3,16 @@ feature 'testing spaces' do
       visit('/spaces')
       expect(page).to have_content("space_1")
     end
+
+    scenario 'adding a space' do
+      visit('/spaces/new')
+      fill_in('space_name', with: 'Space Mansion')
+      fill_in('price', with: '£50')
+      fill_in('description', with: 'Lovely space!')
+      click_button('Submit')
+      expect(page).to have_content('Space Mansion')
+      expect(page).to have_content('£50')
+      expect(page).to have_content('Lovely space!')
+    end
 end
 
