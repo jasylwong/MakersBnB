2941bf7d9a68b1819b8c572014f5997e6b85b998
Ovy95 <jackoverton21@hotmail.com>
2020-02-25 17:12:15 +0000

    Test database environment

---
+++ spec/setup_test_database.rb
@@ -0,0 +1,8 @@
+require "pg"
+def setup_test_database
+p "setting up database"
+
+connection = PG.connect(dbname: "makers_bnb_test")
+
+connection.exec("TRUNCATE spaces,listings,users,bookings; ")
+end
